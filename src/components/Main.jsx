2dafaeb9703181cb4cd3c95a2fb6420ab1088978
Steven Chung <stevenchung.213@gmail.com>
2019-03-09 20:32:00 -0800

    disabled window size check function in componentWillMount

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -53,8 +53,8 @@ export default class Main extends Component {
 
   componentWillMount() {
     this.getLocalStorageUser();
-    this.checkWindowSize();
-    window.addEventListener("resize", this.checkWindowSize);
+    // this.checkWindowSize();
+    // window.addEventListener("resize", this.checkWindowSize);
   }
 
   // componentWillUnmount() {
